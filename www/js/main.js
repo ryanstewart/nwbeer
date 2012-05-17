65199aea69c3f09e0a9209044f33eed063344e74
Ryan Stewart <ryan@ryanstewart.net>
2012-05-17 10:46:06 -0500

    Took out app stuff

--- www/js/main.js
+++ www/js/main.js
@@ -4,45 +4,6 @@
 
 /*global $, document, window, console, Camera, navigator */
 
-function onSuccess(imageData) {
-	'use strict';
-    console.log("success");
-}
-
-function onFail(error) {
-    'use strict';
-    console.log(error.code);
-}
-
-function getCamera() {
-    'use strict';
-    
-	var options = {
-		quality: 50,
-		destinationType : Camera.DestinationType.FILE_URI,
-		allowEdit: true
-	};
-
-	navigator.camera.getPicture(onSuccess, onFail, options);
-}
-
-function onDeviceReady() {
-    'use strict';
-    
-    var $cameradiv = $("<div id='camera-button'>");
-    $cameradiv.addClass("iconButton");
-    $cameradiv.append("Send Beer Photo");
-    $cameradiv.on("click", function (e) {
-        getCamera();
-    });
-    
-    $("#mainpage").append($cameradiv);
-}
-
-function init() {
-    document.addEventListener("deviceready", onDeviceReady, false);    
-}
-
 $(document).ready(function () {
     'use strict';
     
