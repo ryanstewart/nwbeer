8a921cb9478badc4937d6430fc6d99b599e10b67
Ryan Stewart <ryan@ryanstewart.net>
2012-05-17 00:12:04 -0700

    inital commit

---
+++ grunt.js
@@ -0,0 +1,68 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // the staging directory used during the process
+    staging: 'intermediate',
+    // final build output
+    output: 'publish',
+    // filter any files matching one of the below pattern during mkdirs task
+    // the pattern in the .gitignore file should work too.
+    exclude: '.git* build/** node_modules/** grunt.js package.json *.md'.split(' '),
+    mkdirs: {
+      staging: '<config:exclude>'
+    },
+    // concat css/**/*.css files, inline @import, output a single minified css
+    css: {
+      'css/style.css': ['css/**/*.css']
+    },
+    // Renames JS/CSS to prepend a hash of their contents for easier
+    // versioning
+    rev: {
+      js: 'js/**/*.js',
+      css: 'css/**/*.css',
+      img: 'img/**'
+    },
+    // update references in html to revved files
+    usemin: {
+      files: ['**/*.html']
+    },
+    // html minification
+    html: '<config:usemin>',
+    // Optimizes JPGs and PNGs (with jpegtran & optipng)
+    img: {
+      dist: '<config:rev.img>'
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    
+    lint: {
+      files: ['grunt.js', 'js/**/*.js', 'test/**/*.js']
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true
+      }
+    }
+  });
+
+};
---
+++ js/main.js
@@ -0,0 +1,67 @@
+/* Author:
+
+*/
+
+/*global $, document, window, console, Camera, navigator */
+
+function onSuccess(imageData) {
+	'use strict';
+
+
+    var image = document.getElementById('image');
+    image.src = imageData;
+    
+	document.getElementById('takephoto').className = 'animate';
+	document.getElementById('photodetail').className = 'animate';
+	
+    // image.src = "data:image/jpeg;base64," + imageData;
+}
+
+function onFail(error) {
+    'use strict';
+    console.log(error.code);
+}
+
+function getCamera() {
+    'use strict';
+    
+	var options = {
+		quality: 50,
+		destinationType : Camera.DestinationType.FILE_URI,
+		allowEdit: true
+	};
+
+	navigator.camera.getPicture(onSuccess, onFail, options);
+}
+
+$(document).ready(function () {
+    'use strict';
+    
+    var viewportWidth = $(window).width(),
+        viewportHeight = $("#viewport").height();
+    
+    $("#viewport").width(viewportWidth);
+    $("#content").width(viewportWidth * 7);
+    //$("#content").height(viewportHeight);
+
+    $("#content").children().each(function () {
+        $(this).width(viewportWidth);
+    });
+    
+    //console.log(viewportWidth);
+    
+    $(".beer-list").on("click", function (e) {
+        $("#content").css("left", -viewportWidth * 2);
+    });
+
+    $(".the-mountainbeers").on("click", function (e) {
+        $("#content").css("left", -viewportWidth * 6);
+    });
+
+    $(document).on('deviceready', function (e) {
+
+    });
+    
+});
+
+
